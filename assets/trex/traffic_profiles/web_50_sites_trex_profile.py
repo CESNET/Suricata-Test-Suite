b283e6c8c7d6fddd61f488b24911252f63398c03
Matyas Sedmidubsky <sedmidubsky@cesnet.cz>
2026-06-15 16:20:03 +0200

    pytest: add realistic traffic trex profile

---
+++ assets/trex/traffic_profiles/web_50_sites_trex_profile.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+
+from util.config_builder import ConfigBuilder
+
+from .trex_client_manager import BaseTrexClientManager
+
+# taken from web_50_sites/profile.yaml
+pcaps = [
+    ("web_50_sites/dns.pcap", 51),
+    ("web_50_sites/google-cloud.pcap", 1),
+    ("web_50_sites/googlecloud_210315.pcap", 1),
+    ("web_50_sites/pornhub.pcap", 1),
+    ("web_50_sites/amazon_services.pcap", 1),
+    ("web_50_sites/apple_news.pcap", 1),
+    ("web_50_sites/google-maps.pcap", 1),
+    ("web_50_sites/google_play_gquic.pcap", 1),
+    ("web_50_sites/googleplay_210622.pcap", 1),
+    ("web_50_sites/itunes.pcap", 1),
+    ("web_50_sites/xvideos.pcap", 1),
+    ("web_50_sites/xvideos_210601.pcap", 1),
+    ("web_50_sites/akamai_cloud_bbc.pcap", 1),
+    ("web_50_sites/instagram_video.pcap", 1),
+    ("web_50_sites/instagram_210607.pcap", 1),
+    ("web_50_sites/pornhub-network.pcap", 1),
+    ("web_50_sites/youtube_quic.pcap", 1),
+    ("web_50_sites/youtube_210623.pcap", 1),
+    ("web_50_sites/facebook_cloud.pcap", 1),
+    ("web_50_sites/facebookcloud_video.pcap", 1),
+    ("web_50_sites/hulu.pcap", 1),
+    ("web_50_sites/facebook.pcap", 1),
+    ("web_50_sites/facebook_210625.pcap", 1),
+    ("web_50_sites/snapchat.pcap", 1),
+    ("web_50_sites/snapchat_210609.pcap", 1),
+    ("web_50_sites/snapchat_video.pcap", 1),
+    ("web_50_sites/google_services.pcap", 1),
+    ("web_50_sites/netflix_video.pcap", 1),
+    ("web_50_sites/netflix_210611.pcap", 1),
+    ("web_50_sites/instagram.pcap", 1),
+    ("web_50_sites/instagram_210607.pcap", 1),
+    ("web_50_sites/apple-services.pcap", 1),
+    ("web_50_sites/facetime.pcap", 1),
+    ("web_50_sites/facetime_210219.pcap", 1),
+    ("web_50_sites/fastly.pcap", 1),
+    ("web_50_sites/google_ads.pcap", 1),
+    ("web_50_sites/googleads_210622.pcap", 1),
+    ("web_50_sites/google_apis.pcap", 1),
+    ("web_50_sites/googleapis_210620.pcap", 1),
+    ("web_50_sites/pandora.pcap", 1),
+    ("web_50_sites/pandora-audio.pcap", 1),
+    ("web_50_sites/pinterest.pcap", 1),
+    ("web_50_sites/playstation.pcap", 1),
+    ("web_50_sites/playstation_210620.pcap", 1),
+    ("web_50_sites/twitch.pcap", 1),
+    ("web_50_sites/twitch_210615.pcap", 1),
+    ("web_50_sites/whatsapp.pcap", 1),
+    ("web_50_sites/whatsapp_210624.pcap", 1),
+    ("web_50_sites/xbox.pcap", 1),
+    ("web_50_sites/yahoo.pcap", 1),
+    ("web_50_sites/youtube-music.pcap", 1),
+    ("web_50_sites/youtubemusic_210625.pcap", 1),
+]
+
+
+class Web50SitesProfile(BaseTrexClientManager, pcaps=pcaps):
+    def get_stf_profile(self) -> Path:
+        return self.PCAP_PATH_PREFIX / "web_50_sites" / "profile.yaml"
+
+    def stf_config_hook(self, config: ConfigBuilder) -> ConfigBuilder:
+        config.add_option("[0].memory.traffic_mbuf_2048", 128_000)
+        return config
+
+    def get_remote_data_path(self, local_path: Path) -> Path:
+        return Path(f"/opt/trex/{self.trex_version}/web_50_sites") / local_path.name
---
+++ tests/web_50_sites/test_web_50_sites.py
@@ -0,0 +1,104 @@
+"""
+Author(s): Matyáš Sedmidubský <sedmidubsky@cesnet.cz>
+
+Copyright: (C) 2026 CESNET, z.s.p.o.
+
+Suricata testing module.
+"""
+
+import pytest
+import signal
+
+from typing import List
+from lbr_testsuite import trex
+from assets.trex.traffic_profiles.trex_client_manager import TrexMode
+from assets.trex.traffic_profiles.web_50_sites_trex_profile import Web50SitesProfile
+from util.suricata_manager import Suricata_manager, SuriDown
+from util.suri_util import save_stats, TestInfo, RunInfo
+from conftest import kill_pytest, get_trex_multi, suri_interface_bind, Suri_conf
+
+
+@pytest.mark.parametrize(
+    "rules_config",
+    [
+        {"name": "norules", "path": "/dev/null/"},
+        {"name": "rules", "path": "/var/lib/suricata/rules/suricata.rules"},
+    ],
+    ids=["norules", "rules"],
+)
+def test_web_50_sites(
+    request: pytest.FixtureRequest,
+    trex_generators: dict,
+    result_path: str,
+    suricata_tmp_stats_path: str,
+    utilized_programs_info: dict,
+    params: dict,
+    suri_conf: Suri_conf,
+    get_settings_file: str,
+    get_traffic_duration: int,
+    get_heatup_duration: int,
+    rules_config: dict,
+    get_target_mac: str,
+    get_target_vlan: int,
+):
+
+    trex_manager: trex.TRexManager = trex.TRexManager(
+        trex.TRexMachinesPool(trex_generators)
+    )
+
+    suri_daemon: Suricata_manager = Suricata_manager(
+        request,
+        suricata_tmp_stats_path,
+        interface=suri_interface_bind(request)[0],
+        capture_mode=suri_interface_bind(request)[1],
+        conf_file=suri_conf.conf_file.with_params(params).build(),
+        rules_file=rules_config["path"],
+    )
+    signal.signal(signal.SIGINT, kill_pytest)
+
+    test_info = TestInfo(
+        result_path=result_path,
+        traffic_duration=get_traffic_duration,
+        heatup_duration=get_heatup_duration,
+        suricata_path_to_bin=suri_daemon.get_path_to_binary(),
+        suricata_rules_paths=[suri_daemon.rules_file],
+        suricata_config_path=suri_daemon.conf_file,
+        utilized_programs_info=utilized_programs_info,
+    )
+
+    trex_client = Web50SitesProfile(
+        trex_manager, request, get_target_mac, get_target_vlan, mode=TrexMode.STF
+    )
+
+    test_variant_name = f"{suri_conf.test_name}_{rules_config['name']}"
+    trex_multipliers: List[float] = get_trex_multi(
+        get_settings_file, suri_conf.server, suri_conf.pcie, test_variant_name
+    )
+
+    for idx, multiplier in enumerate(trex_multipliers, 1):
+        run_info = RunInfo(multiplier=multiplier)
+
+        print(
+            f"\n[Progress] multiplier {idx}/{len(trex_multipliers)} | param_file={request.config.getoption('--param-file')} | params={params}"
+        )
+        print(f"sending packets at {run_info.multiplier} * default cps of .pcap")
+
+        trex_client.set_props(run_info.multiplier, test_info.traffic_duration)
+        trex_client.prepare()
+
+        try:
+            suri_daemon.start()
+        except SuriDown:
+            pytest.fail("Suricata is down.")
+
+        trex_client.run()
+
+        try:
+            suri_daemon.stop()
+        except SuriDown:
+            pytest.fail("Suricata was down.")
+
+        trex_client.update_runinfo(run_info)
+        run_info.suricata_start_delay = suri_daemon.last_start_delay
+
+        save_stats(params, request, test_info, run_info)
