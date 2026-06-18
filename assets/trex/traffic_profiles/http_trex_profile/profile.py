7594d1ad9e9bc30e73e20e96b77c48f4d29a30c8
Matyas Sedmidubsky <sedmidubsky@cesnet.cz>
2026-06-18 17:28:32 +0200

    pytest: convert lbr_testsuite profiles to native ASTF format

---
+++ assets/trex/traffic_profiles/http_trex_profile/profile.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import lbr_trex_client.interactive.trex.astf.trex_astf_profile as astf_profile
+from assets.trex.traffic_profiles.trex_client_manager import BaseTrexClientManager
+
+from .native import Prof1
+
+native = Prof1()
+defaults = native.pcaps
+
+
+class HttpProfile(BaseTrexClientManager, pcaps=native.pcaps):
+    def get_astf_profile(self, multiplier: float) -> astf_profile.ASTFProfile:
+        for i in range(len(native.pcaps)):
+            cap = Path(__file__).parent.parent / "pcaps" / defaults[i][0]
+            cps = defaults[i][1] * multiplier
+            native.pcaps[i] = (str(cap), cps)
+
+        profile = native.get_profile({})
+
+        return profile
